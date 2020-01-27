1933a04f68afe9dbb645adaf113d70cf8b88c612
Wang Juntao <wangjt@zetyun.com>
2020-01-27 17:29:44 -0500

    Add a tree to the city

--- 01-python/infrastructure/tree.py
+++ 01-python/infrastructure/tree.py
@@ -1,3 +1,8 @@
-def draw_tree():
-    print("tree not found")
+def draw_tree(height=5):
+    if height<=0:
+        print("tree not found")
+    else:
+        for i in range(height):
+            print(' ' * (height - i) + '*' * (2 * i + 1))
+        print(' ' * height + "|")
     return
